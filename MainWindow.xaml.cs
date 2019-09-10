b1d29a6c4b2f286e0a5380cf573891d2e62d604d
CasaSpider <peter.nijhuis@brabantwater.nl>
2019-09-10 20:34:46 +0200

    Added button => CopyTheFile, StartTheProgram.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,69 +1,97 @@
 ﻿using System.ComponentModel;
 using System.Windows;
 using System.Threading;
+using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+using System.IO;
 
-namespace WpfApplication1 {
-    /// <summary>
-    ///   Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+
+namespace WpfApplication1
+{
+
+    public partial class MainWindow : Window
+    {
+
+        public static List<string> PhaseList = new List<string>() { " ", "T", "A", "P" };
+        public static string Phase = string.Empty;
+
+        public static string BaseDir = @"D:\Temp\etc";
+        public static string Program2Run = @"C:\Windows\System32\calc.exe";
+
+        public MainWindow()
+        {
             InitializeComponent();
 
+            cbx_phase.ItemsSource = PhaseList;
+            cbx_phase.SelectedIndex = 0;
+
             var viewModel = new ViewModel();
-            viewModel.Phase = "A";
+            viewModel.Phase = cbx_phase.Text;
             DataContext = viewModel;
 
-            Loaded += delegate {
-                var thread = new Thread(() => {
-                while (true) {
-                    Thread.Sleep(1000);
-                    //viewModel.Count++;
-                    if (viewModel.Phase == "D")
-                        viewModel.Phase = "A";
-                    else
-                        viewModel.Phase = "D";
+            Loaded += delegate
+            {
+                var thread = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        viewModel.Phase = Phase;
                     };
-                }) {
+                })
+                {
                     IsBackground = true
                 };
                 thread.Start();
             };
         }
+
+        private void CopyTheFile()
+        {
+            string from = $@"{BaseDir}\hosts_{Phase}";
+            string to = $@"{BaseDir}\hosts";
+            File.Copy(from, to, true);
+        }
+
+        private void StartTheProgram()
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.Arguments = string.Empty;
+            start.FileName = Program2Run;
+            start.WindowStyle = ProcessWindowStyle.Hidden;
+            start.CreateNoWindow = true;
+            int exitCode;
+
+            using (Process proc = Process.Start(start))
+            {
+                proc.WaitForExit();
+                exitCode = proc.ExitCode;
+            }
+        }
+
+        private void cbx_dropdownclosed(object sender, EventArgs e)
+        {
+            Phase = cbx_phase.Text;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            CopyTheFile();
+            StartTheProgram();
+        }
     }
 
-    public enum Phases
+    public class ViewModel : INotifyPropertyChanged
     {
-        Development,
-        Test,
-        Acceptance,
-        Production,
-    }
-
-    public class ViewModel : INotifyPropertyChanged {
-        private int _count;
         private string _phase;
         public event PropertyChangedEventHandler PropertyChanged;
 
-        //public int Count {
-        //    get { return _count; }
-        //    set {
-        //        if (_count != value) {
-        //            _count = value;
-        //            var handler = PropertyChanged;
-        //            if (handler != null) {
-        //                handler(this, new PropertyChangedEventArgs("Count"));
-        //            }
-        //        }
-        //    }
-        //}
-
         public string Phase
         {
-            get { return _phase;  }
+            get { return _phase; }
             set
             {
-                if(_phase != value)
+                if (_phase != value)
                 {
                     _phase = value;
                     var handler = PropertyChanged;
